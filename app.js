362c0e7205984afbc060df820a6208a3b90672ee
Malcolm Bird <bird.malcolm@gmail.com>
2014-02-22 19:13:06 +0900

    updated

--- app.js
+++ app.js
@@ -1,9 +1,34 @@
 $(document).ready(function() {
 
+var newNumber = prompt("Please enter an integer between 1 and 100");
+
+
+var TestNewNumber = function(){
+
+	while (newNumber % 1 !== 0 || newNumber > 100 || newNumber < 1) {
+	
+	
+	if (newNumber === ""){
+		newNumber = prompt("Try again. Please enter an integer between 1 and 100");
+	}
+	else if (newNumber > 100 || newNumber < 1) {
+		newNumber = prompt("That's not between 1 and 100! Please try again.");
+	}
+	else if (newNumber % 1 !== 0) {
+		newNumber = prompt("Uh oh! That's not a whole number! Enter a NUMBER without fractions or decimals! Please try again.");
+	
+	}
+
+}
+};
+
+TestNewNumber();
+
 count = 1;
 
 
-for (count = 1; count <= 100; count++) {
+var FizzBuzz = function() {
+	for (count = 1; count <= +newNumber; count++) {
 	if (count % 3 === 0){
 		if (count % 5 === 0){
 			$("body").append("fizzbuzz, ");
@@ -15,8 +40,10 @@ for (count = 1; count <= 100; count++) {
 	} else {
 		$("body").append(count + ", ");
 	}
-}
+	}
+};
 
+FizzBuzz();
 
 
 });
